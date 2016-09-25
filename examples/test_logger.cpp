8018fe1c89e0b4aceb85bfc03a73cc02cc32387c
leviathan1995 <leviathan1995@outlook.com>
2016-09-26 00:38:03 +0800

    update test_logger.cpp

--- examples/test_logger.cpp
+++ examples/test_logger.cpp
@@ -6,8 +6,6 @@
 //  Copyright © 2016年 leviathan. All rights reserved.
 //
 
-/*
-
 #include <stdio.h>
 #include "luves.h"
 using namespace luves;
@@ -18,11 +16,7 @@ int main()
     fatal("fatal test");
     fatalif(1,"fataif test");
     check(1, "check test");
-    
     //exitif(1, "exitif test");
-    
     fatalif(-1,"-1");
     fatalif(0,"0");
 }
-
-*/
