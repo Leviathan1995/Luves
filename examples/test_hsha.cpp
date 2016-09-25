b000358bd73478ec6865c9298af8211d0c33b155
leviathan1995 <leviathan1995@outlook.com>
2016-09-26 02:01:07 +0800

    reanme variable

--- examples/test_hsha.cpp
+++ examples/test_hsha.cpp
@@ -5,7 +5,7 @@
 //  Created by leviathan on 16/6/28.
 //  Copyright © 2016年 leviathan. All rights reserved.
 //
- 
+
 #include "luves.h"
 
 using namespace luves;
@@ -19,18 +19,18 @@ int main()
 {
     EventLoop loop;
     Ip4Addr server_addr("127.0.0.1",8080);
-    
-    
-    HshaServer server(&loop, server_addr,4);
+
+
+    HSHAServer server(&loop, server_addr,4);
     server.SetReadCb(GetInput);
-    
+
     server.SetWriteCb([](const TcpConnectionPtr & conn)
                       {conn->Send("HTTP/1.1 200 OK\r\n"
                                   "Content-Type:text/html;charset=utf-8\r\n"
                                   "Content-Length:18\r\n"
                                   "\r\n"
                                   "Welcome to tinyweb");});
-    
+
     server.RunServer();
     loop.loop();
 }
