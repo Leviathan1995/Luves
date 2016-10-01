70e82d08dc596bb6a6c09a5b5ea5e7d9d8f0b817
leviathan1995 <leviathan1995@outlook.com>
2016-10-01 22:32:42 +0800

    fix memory leak bug

--- luves/net.cpp
+++ luves/net.cpp
@@ -21,21 +21,21 @@ namespace luves
             return errno;
         return fcntl(fd, F_SETFL,flags|O_NONBLOCK);
     }
-    
+
     int SocketOp::SetReuseaddr(int fd)
     {
         int flags=1;
         int len=sizeof(flags);
         return setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &flags, len);
     }
-    
+
     int SocketOp::SetReuseport(int fd)
     {
         int flags=1;
         int len=sizeof(flags);
         return setsockopt(fd, SOL_SOCKET, SO_REUSEPORT, &flags, len);
     }
-    
+
     //
     //Ip4地址模块
     //
@@ -46,7 +46,7 @@ namespace luves
         sprintf(str, "%d.%d.%d.%d:%d",(host>> 0)&0xff,(host >> 8)&0xff,(host >> 16)&0xff,(host>> 24)&0xff, ntohs(addr_.sin_port));
         return str;
     }
-    
+
     //
     //Socket
     //
@@ -54,19 +54,19 @@ namespace luves
     {
         Socket::Close(fd_);
     }
-    
+
     void Socket::Close(int fd)
     {
         close(fd);
     }
-    
+
     int Socket::CreateNonBlockSocket()
     {
         int socket_fd=socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
         FATALIF_LOG(!socket_fd,"socket fd create failed!");
         return socket_fd;
     }
-    
+
     int Socket::Bind(int fd,sockaddr_in * server_addr)
     {
         int ret =bind(fd,(struct sockaddr *)server_addr,sizeof(*server_addr));
@@ -84,10 +84,10 @@ namespace luves
         int ret=listen(fd, 20);
         if (ret==-1)
         {
-            FATALIF_LOG(ret,"listen to %d failed! %d %s",errno,strerror(errno));
+            ERROR_LOG("listen to %d failed! %d %s",fd,errno,strerror(errno));
         }
     }
-    
+
     int Socket::Accept(int fd,sockaddr_in * client_addr)
     {
         socklen_t client_len=sizeof(*client_addr);
@@ -96,11 +96,11 @@ namespace luves
             ERROR_LOG("accept to %d failed! %d %s",fd,errno,strerror(errno));
         return accept_fd;
     }
-    
+
     void Socket::Connect(int fd,sockaddr_in * server_addr)
     {
         connect(fd, (struct sockaddr *)&server_addr, sizeof(server_addr));
 
     }
-    
+
 }
